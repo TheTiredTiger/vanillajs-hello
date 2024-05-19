85ef4c0c9c120256fd144c31c192d82fa93fdc91
Bárbara Ferreira <166528369+TheTiredTiger@users.noreply.github.com>
2024-05-19 20:38:15 +0000

    Project #4: JS Excuse Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("button").addEventListener("click", () => {
+    document.getElementById("excuse").innerHTML = excuseGenerator();
+  });
 };
+
+function excuseGenerator() {
+  let who = ["A genie", "A sorcerer", "Zeus", "My cat", "My grandma"];
+  let action = [
+    "spirited away",
+    "zapped",
+    "crushed",
+    "exploded",
+    "devoured",
+    "yeeted"
+  ];
+  let what = [
+    "my spellbook",
+    "my dragon",
+    "my broom",
+    "my dignity",
+    "the last cookie in the jar",
+    "my one remaining kidney"
+  ];
+  let when = [
+    "before class.",
+    "while I was doing the splits.",
+    "while I was being arrested.",
+    "during therapy.",
+    "while I explored Mars.",
+    "after our 5'oclock tea."
+  ];
+
+  let subject = Math.floor(Math.random() * who.length);
+  let verb = Math.floor(Math.random() * action.length);
+  let object = Math.floor(Math.random() * what.length);
+  let tempClause = Math.floor(Math.random() * when.length);
+
+  return `${who[subject]} ${action[verb]} ${what[object]} ${when[tempClause]}`;
+}
