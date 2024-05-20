3e4937f26522521c5918e83fcc5f1d49cc8a7112
Bárbara Ferreira <166528369+TheTiredTiger@users.noreply.github.com>
2024-05-20 19:25:44 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let determiner = ["the", "our", "this", "that"];
+let adj = [
+  "great",
+  "big",
+  "marvelous",
+  "first",
+  "last",
+  "incredible",
+  "blooming",
+  "suspicious",
+  "inconspicuous"
+];
+let noun = ["jogger", "racoon", "wizard", "beluga", "ostrich", "dentist"];
+let extension = [".com", ".net", ".io", ".pt", ".co.uk"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function domainGenerator(arr1, arr2, arr3, arr4) {
+  let domains = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let a = 0; a < arr1.length; a++) {
+    for (let b = 0; b < arr2.length; b++) {
+      for (let c = 0; c < arr3.length; c++) {
+        for (let d = 0; d < arr4.length; d++) {
+          domains.push(arr1[a] + arr2[b] + arr3[c] + arr4[d]);
+        }
+      }
+    }
+  }
+  console.log(domains);
+}
+
+domainGenerator(determiner, adj, noun, extension);
